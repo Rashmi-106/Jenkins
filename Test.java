09ed933ca749c210226c7acc57b46f30ffbcd029
Rashmi-106 <rashmip036@gmail.com>
2025-08-29 10:40:40 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
-public static Test{
-            public static void main(String[] args){
-                        System.out.println("Hello World");
-            }
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
